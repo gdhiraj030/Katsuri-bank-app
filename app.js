936d25fc71bf982f341cee797d114eabfa65b7ef
dhiraj gurung <gdhiraj030@gmail.com>
2025-01-12 00:56:52 +0200

    complete html and css

---
+++ app.js
@@ -0,0 +1,29 @@
+"use strict";
+// message and total
+const labelWelcome = document.querySelector(".label--welcome");
+const totalBalance = document.querySelector(".total--balance");
+
+// transaction 
+const transactionHistory = document.querySelector(".transaction__history");
+const transactionRow = document.querySelector(".transaction__row");
+const typeDeposited = document.querySelector(".type--deposited");
+const typeWithdrawal = document.querySelector(".type--withdrawal");
+
+// label summary
+const labelIncome = document.querySelector(".label--income");
+const labelOutcome = document.querySelector(".label--outcome");
+const btnSort = document.querySelector(".sort--btn");
+
+// operation inputs
+const receiverInput = document.querySelector(".receiver--input");
+const loanInput = document.querySelector(".loan--input");
+const closeInput = document.querySelector(".close--input");
+
+// operation password
+const receiverPassword = document.querySelector(".receiver--password");
+const closePassword = document.querySelector(".close--password");
+
+// operation button btn
+const sendBtn = document.querySelector(".send--btn");
+const loanBtn = document.querySelector(".loan--btn");
+const closeBtn = document.querySelector(".close--btn");
